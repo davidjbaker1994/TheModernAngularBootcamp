c7c49a77c47793e0aa96ada9bf9b087c2bcd1543
Jake BAKER <E228048@na.int.grp>
2022-05-10 10:51:13 -0400

    114. Implementing a Titled Divider

--- comps/src/app/elements/elements.module.ts
+++ comps/src/app/elements/elements.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Given that SharedModule is now imported here, any component defined in 'elements' directory
+//can access / make reference to any component defined in the 'shared' directory
+import { SharedModule } from '../shared/shared.module';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
@@ -15,7 +18,8 @@ import { TimesDirective } from './times.directive';
   ],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule
   ]
 })
 export class ElementsModule { }
